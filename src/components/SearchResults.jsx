ad9c5cd6044ae6becc946c414ba1eb2c457cd324
Joshua Ramirez <Joshua Ramirez>
2017-01-27 15:22:55 -0600

    Added an image 404 handler in search results.

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import AppBus from '../AppBus';
 import SearchResultsStore from "../stores/SearchResults"
 
+const onImageError = function(event){
+  event.currentTarget.onerror = "";
+  event.currentTarget.src = "http://placehold.it/100x150?text=404";
+  return true;
+};
+
 class SearchResult extends Component {
   constructor(props) {
     super(props);
@@ -21,15 +27,15 @@ class SearchResult extends Component {
               <div className="well clearfix" key={movie.imdbId}>
                 <div className="pull-left">
                   <div className="movieResult thumbnail">
-                      <img className="img-responsive" src={movie.poster} role="presentation"/>
+                      <img className="img-responsive" src={movie.poster} role="presentation" onError={onImageError.bind(this)}/>
                   </div>
                 </div>
                 <div>
                   <h3>{movie.title}</h3>
-                  Year: {movie.year}
+                  <div>Year: {movie.year}</div>
                 </div>
                 <br/>
-                <button type="button" className="btn btn-primary pull-right block">Select</button>
+                <button type="button" className="btn btn-primary block">Select</button>
               </div>
             );
           })
