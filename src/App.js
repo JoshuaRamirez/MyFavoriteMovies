2e591d8caa033c8d1b3bd48d1f30bb64696fb403
Joshua Ramirez <Joshua Ramirez>
2017-01-27 10:39:34 -0600

    Extracted header html to new header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header.jsx';
 
 class App extends Component {
   render() {
@@ -7,33 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="container-fluid">
 
-          <nav className="navbar navbar-default navbar-fixed-top navbar-margin" role="navigation">
-            <div className="container">
-              <div className="navbar-header">
-                  <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                      <span className="sr-only">Toggle navigation</span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                  </button>
-                  <a className="navbar-brand" href="#">My Favorite Movies</a>
-                  <button type="button" className="btn btn-default navbar-btn" data-toggle="modal" data-target="#findMovie">
-                    <span className="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-                    &nbsp;Add Movie
-                  </button>
-              </div>
-              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <form className="navbar-form navbar-right" role="search">
-                  <div className="form-group">
-                    <div className="input-group">
-                      <input type="text" className="form-control" placeholder="Search My Favorites"/>
-                      <div className="input-group-addon"><span className="glyphicon glyphicon-search" aria-hidden="true"></span></div>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </nav>
+          <Header />
 
           <div className="modal fade" tabIndex="-1" role="dialog" id="findMovie">
             <div className="modal-dialog" role="document">
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top navbar-margin" role="navigation">
+        <div className="container">
+          <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">My Favorite Movies</a>
+              <button type="button" className="btn btn-default navbar-btn" data-toggle="modal" data-target="#findMovie">
+                <span className="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
+                &nbsp;Add Movie
+              </button>
+          </div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <form className="navbar-form navbar-right" role="search">
+              <div className="form-group">
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="Search My Favorites"/>
+                  <div className="input-group-addon"><span className="glyphicon glyphicon-search" aria-hidden="true"></span></div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
