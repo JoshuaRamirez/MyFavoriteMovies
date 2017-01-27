adfb6ee59c239345bdacddb128ff0abad644ebcf
Joshua Ramirez <Joshua Ramirez>
2017-01-27 10:52:42 -0600

    Extract the find movie modal html to its own component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header.jsx';
 import FavoriteMovie from './components/FavoriteMovie.jsx';
+import FindMovie from './components/FindMovie.jsx';
 
 class App extends Component {
   render() {
@@ -11,111 +12,7 @@ class App extends Component {
 
           <Header />
 
-          <div className="modal fade" tabIndex="-1" role="dialog" id="findMovie">
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                  <h4 className="modal-title header-margin">Find Movie by Title</h4>
-                  <div className="form-group">
-                    <div className="input-group">
-                      <input type="text" className="form-control" placeholder="Movie Title"/>
-                      <div className="input-group-addon">
-                        <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="modal-body whitesmoke">
-                  <div className="well clearfix">
-
-                    <div className="pull-left">
-                      <div className="movieResult thumbnail">
-                          <img className="img-responsive" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMGQwNDNkMmItYWY1Yy00YTZmLWE5OTAtODU0MGZmMzQ1NDdkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" role="presentation"/>
-                      </div>
-                    </div>
-
-                    <div>
-                      <h3>This is the movie title</h3>
-                      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-                    </div>
-                    <br/>
-                    <button type="button" className="btn btn-primary pull-right block">Select</button>
-
-                  </div>
-                  <div className="well clearfix">
-
-                    <div className="pull-left">
-                      <div className="movieResult thumbnail">
-                          <img className="img-responsive" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMGQwNDNkMmItYWY1Yy00YTZmLWE5OTAtODU0MGZmMzQ1NDdkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" role="presentation"/>
-                      </div>
-                    </div>
-
-                    <div>
-                      <h3>This is the movie title</h3>
-                      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-                    </div>
-                    <br/>
-                    <button type="button" className="btn btn-primary pull-right block">Select</button>
-
-                  </div>
-                  <div className="well clearfix">
-
-                    <div className="pull-left">
-                      <div className="movieResult thumbnail">
-                          <img className="img-responsive" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMGQwNDNkMmItYWY1Yy00YTZmLWE5OTAtODU0MGZmMzQ1NDdkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" role="presentation"/>
-                      </div>
-                    </div>
-
-                    <div>
-                      <h3>This is the movie title</h3>
-                      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-                    </div>
-                    <br/>
-                    <button type="button" className="btn btn-primary pull-right block">Select</button>
-
-                  </div>
-                  <div className="well clearfix">
-
-                    <div className="pull-left">
-                      <div className="movieResult thumbnail">
-                          <img className="img-responsive" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMGQwNDNkMmItYWY1Yy00YTZmLWE5OTAtODU0MGZmMzQ1NDdkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" role="presentation"/>
-                      </div>
-                    </div>
-
-                    <div>
-                      <h3>This is the movie title</h3>
-                      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-                    </div>
-                    <br/>
-                    <button type="button" className="btn btn-primary pull-right block">Select</button>
-
-                  </div>
-                  <div className="well clearfix">
-
-                    <div className="pull-left">
-                      <div className="movieResult thumbnail">
-                          <img className="img-responsive" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMGQwNDNkMmItYWY1Yy00YTZmLWE5OTAtODU0MGZmMzQ1NDdkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" role="presentation"/>
-                      </div>
-                    </div>
-
-                    <div>
-                      <h3>This is the movie title</h3>
-                      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
-                    </div>
-                    <br/>
-                    <button type="button" className="btn btn-primary pull-right block">Select</button>
-
-                  </div>
-                </div>
-
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <FindMovie />
 
           <div className="row content-margin">
             <FavoriteMovie />
