6e8af389e4bef494d22e3b2e8adcf2975f7d3bd2
Joshua Ramirez <Joshua Ramirez>
2017-01-27 19:32:42 -0600

    Added Events constants.

---
+++ src/Events.js
@@ -0,0 +1,9 @@
+export default {
+  Ajax: {
+    MovieDetailsFound: "MovieDetailsFound",
+    SearchResultsFound: "SearchResultsFound"
+  },
+  Gestures: {
+    AddMovieRequested: "AddMovieRequested"
+  }
+};
