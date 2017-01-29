f235d31677d20086150eef6659fcef3a162bff6e
Joshua Ramirez <Joshua Ramirez>
2017-01-28 19:39:30 -0600

    Added read-only state switching on fields.

--- src/components/FavoriteMovie.jsx
+++ src/components/FavoriteMovie.jsx
@@ -5,20 +5,36 @@ class FavoriteMovie extends Component {
     super(props);
     props.movie.isReadOnly = true;
     this.state = props.movie;
+    this.onEditClicked = this.onEditClicked.bind(this);
+    this.onSaveClicked = this.onSaveClicked.bind(this);
+    this.onDeleteClicked = this.onDeleteClicked.bind(this);
+  }
+  onEditClicked(event){
+    event.preventDefault();
+    this.setState({isReadOnly: false});
+  }
+  onSaveClicked(event){
+    event.preventDefault();
+    this.setState({isReadOnly: true});
+  }
+  onDeleteClicked(event){
+    event.preventDefault();
+    this.setState({isReadOnly: true});
   }
   render() {
     const isReadOnly = this.state.isReadOnly;
+    const self = this;
     const renderSaveOrEditButton = function(){
       if (isReadOnly) {
         return (
-          <button className="btn btn-default">
+          <button onClick={self.onEditClicked} className="btn btn-default">
             <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
              &nbsp;Edit
           </button>
         );
       } else {
         return (
-          <button type="submit" className="btn btn-primary" disabled="disabled">
+          <button onClick={self.onSaveClicked} type="submit" className="btn btn-primary">
             <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>
             &nbsp;Save
           </button>
@@ -37,40 +53,40 @@ class FavoriteMovie extends Component {
                 </div>
               </div>
               <div className="col-xs-6">
-                  <form>
-                    <div className="form-group">
-                      <label htmlFor="title">Title:</label>
-                      <input readOnly={isReadOnly} type="text" value={this.state.title} className="form-control" id="title"/>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="genre">Genre:</label>
-                      <input readOnly={isReadOnly} type="text" value={this.state.genre} className="form-control" id="genre"/>
-                    </div>
-                    <div className="row">
-                      <div className="col-xs-6">
-                        <div className="form-group">
-                          <label htmlFor="genre">Year:</label>
-                          <input readOnly={isReadOnly} type="text" value={this.state.year} className="form-control" id="year"/>
-                        </div>
-                      </div>
-                      <div className="col-xs-6">
-                        <div className="form-group">
-                          <label htmlFor="genre">Rating:</label>
-                          <input readOnly={isReadOnly} type="text" value={this.state.imdbRating} className="form-control" id="rating"/>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="genre">Actors:</label>
-                      <textarea readOnly={isReadOnly} type="text" className="form-control" rows="3" id="actors" value={this.state.actors}></textarea>
-                    </div>
-                    {renderSaveOrEditButton()}&nbsp;
-                    <button className="btn btn-danger">
-                      <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                      &nbsp;Delete
-                    </button>
-                  </form>
+                <div className="form-group">
+                  <label htmlFor="title">Title:</label>
+                  <input readOnly={isReadOnly} type="text" value={this.state.title} className="form-control" id="title"/>
                 </div>
+                <div className="form-group">
+                  <label htmlFor="genre">Genre:</label>
+                  <input readOnly={isReadOnly} type="text" value={this.state.genre} className="form-control" id="genre"/>
+                </div>
+                <div className="row">
+                  <div className="col-xs-6">
+                    <div className="form-group">
+                      <label htmlFor="genre">Year:</label>
+                      <input readOnly={isReadOnly} type="text" value={this.state.year} className="form-control" id="year"/>
+                    </div>
+                  </div>
+                  <div className="col-xs-6">
+                    <div className="form-group">
+                      <label htmlFor="genre">Rating:</label>
+                      <input readOnly={isReadOnly} type="text" value={this.state.imdbRating} className="form-control" id="rating"/>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="genre">Actors:</label>
+                  <textarea readOnly={isReadOnly} type="text" className="form-control" rows="3" id="actors" value={this.state.actors}></textarea>
+                </div>
+                <form>
+                  {renderSaveOrEditButton()}&nbsp;
+                  <button onClick={this.onDeleteClicked} className="btn btn-danger">
+                    <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                    &nbsp;Delete
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
           </div>
