ffd9ef00149a0edb47b939d086e88bfb3c1a5d68
JoshuaRamirez <joshuaramirez2048@gmail.com>
2017-01-29 08:04:21 -0600

    Refactoring AppBus for minor fixes.

--- src/AppBus.js
+++ src/AppBus.js
@@ -1,54 +1,50 @@
-function Bus() {
+function AppBus() {
 
-    var subscriptions = [];
+    const subscriptions = [];
 
-    var makeDeliveryJob = function (subscriber) {
+    const makeDeliveryJob = function (subscriber) {
         return function (payload) {
             subscriber.apply(null, [payload]);
         };
     };
 
-    var makeSubscription = function (subscriber, eventName) {
-        var subscription = {
+    const makeSubscription = function (subscriber, eventName) {
+        const send = makeDeliveryJob(subscriber);
+        return {
             Subscriber: subscriber,
             EventName: eventName,
-            Send: makeDeliveryJob(subscriber)
+            Send: send
         };
-        return subscription;
     };
 
-    var sendSubscriptions = function (eventName, payload) {
-        for (var i = 0; i < subscriptions.length; i++) {
-            var subscription = subscriptions[i];
+    const sendSubscriptions = function (eventName, payload) {
+        subscriptions.forEach(function(subscription){
             if (subscription.EventName === eventName) {
                 subscription.Send(payload);
             }
-        }
+        });
     };
 
-    var makeSubscriptionApi = function (subscriber) {
+    const makeSubscriptionApi = function (subscriber) {
 
         //TODO: Ensure unique subscriptions
-
-        //Implementation
-        var to = function (eventName) {
-            var subscription = makeSubscription(subscriber, eventName);
+        const to = function (eventName) {
+            const subscription = makeSubscription(subscriber, eventName);
             if(typeof eventName !== "string"){
               throw new Error("Event name is not a string. Found: " + typeof eventName);
             }
             subscriptions.push(subscription);
         };
 
-        //"API"
         return {
             To: to
         };
 
     };
 
-    var makeUnsubscribeApi = function (subscriber) {
+    const makeUnSubscribeApi = function (subscriber) {
 
-        var from = function (eventName) {
+        const from = function (eventName) {
           if(typeof eventName !== "string"){
             throw new Error("Event name is not a string. Found: " + typeof eventName);
           }
@@ -63,23 +59,23 @@ function Bus() {
           From: from
         };
 
-    }
+    };
 
-    var subscribe = function (subscriber) {
+    const subscribe = function (subscriber) {
       if(typeof subscriber !== "function"){
         throw new Error("Subscriber is not a function. Found: " + typeof subscriber);
       }
       return makeSubscriptionApi(subscriber);
     };
 
-    var unsubscribe = function (subscriber) {
+    const unSubscribe = function (subscriber) {
       if(typeof subscriber !== "function"){
         throw new Error("Subscriber is not a function. Found: " + typeof subscriber);
       }
-      return makeUnsubscribeApi(subscriber);
-    }
+      return makeUnSubscribeApi(subscriber);
+    };
 
-    var publish = function (eventName, payload) {
+    const publish = function (eventName, payload) {
       if(typeof eventName !== "string") {
         throw new Error("Subscriber is not a string. Found: " + typeof subscriber);
       }
@@ -89,9 +85,9 @@ function Bus() {
     return {
         Subscribe: subscribe,
         Publish: publish,
-        UnSubscribe: unsubscribe
+        UnSubscribe: unSubscribe
     };
 
 }
 
-export default Bus();
+export default AppBus();
